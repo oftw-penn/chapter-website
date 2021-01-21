1d979fbf3e828996d074fec1f0a90c60e914fcf1
Chris Elliott <elliott77chris@gmail.com>
2021-01-21 12:16:00 -0800

    Remove unnecessary dependencies.

--- src/main/java/com/oftwpenn/main/Main.java
+++ src/main/java/com/oftwpenn/main/Main.java
@@ -1,12 +1,9 @@
 package com.oftwpenn.main;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEncryptableProperties
 public class Main {
 
 	public static void main(String[] args) {
